07ecb9877fe0abef156d9513fe41ab5648fc8a5f
TinyChou <zhoujinfu0124@163.com>
2023-03-30 22:11:25 +0800

    chore(eslint): add local development auto eslint with devServer on

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
 import vue2 from "@vitejs/plugin-vue2";
 import vue2Jsx from "@vitejs/plugin-vue2-jsx";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // do not fail on serve (i.e. local development)
+    {
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+        cache: true,
+        fix: true,
+      }),
+      apply: "serve",
+      enforce: "post",
+    },
     vue2(),
     vue2Jsx(),
     legacy({
